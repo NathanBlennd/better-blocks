55f52bd69dc8397ceb7695bbf91360b42873a3e7
Nathan Johnson <nathanj.runs@gmail.com>
2023-10-21 10:24:26 -0600

    Allow multiple countdown blocks per page

--- blocks/countdown/ts/countdown.ts
+++ blocks/countdown/ts/countdown.ts
@@ -1,26 +1,33 @@
 window.onload = function () {
 
-	let countdown : HTMLElement = document.querySelector('.countdown');
-	let date = Date.parse( countdown.dataset.date );
+	const COUNTDOWN_SELECTOR = '.countdown';
+	const COUNTDOWN_DAYS_SELECTOR = '.days';
+	const COUNTDOWN_HOURS_SELECTOR = '.hours';
+	const COUNTDOWN_MINUTES_SELECTOR = '.minutes';
+	const COUNTDOWN_SECONDS_SELECTOR = '.seconds';
 
-	let days : HTMLElement = countdown.querySelector('.days');
-	let hours : HTMLElement = countdown.querySelector('.hours');
-	let minutes : HTMLElement = countdown.querySelector('.minutes');
-	let seconds : HTMLElement = countdown.querySelector('.seconds');
+	let countdowns = document.querySelectorAll(COUNTDOWN_SELECTOR);
+	countdowns.forEach( function( countdown : HTMLElement) {
+		let date = Date.parse( countdown.dataset.date );
 
+		let days : HTMLElement = countdown.querySelector(COUNTDOWN_DAYS_SELECTOR);
+		let hours : HTMLElement = countdown.querySelector(COUNTDOWN_HOURS_SELECTOR);
+		let minutes : HTMLElement = countdown.querySelector(COUNTDOWN_MINUTES_SELECTOR);
+		let seconds : HTMLElement = countdown.querySelector(COUNTDOWN_SECONDS_SELECTOR);
 
-	setInterval(function(){
-		let diff = date-Date.now();
-		if( diff < 0 ) {
-			diff = 0;
-		}
-		let diff_as_date = new Date(diff);
+		setInterval(function(){
+			let diff = date-Date.now();
+			if( diff < 0 ) {
+				diff = 0;
+			}
+			let diff_as_date = new Date(diff);
 
-		days.textContent = diff_as_date.getDate().toString();
-		hours.textContent = diff_as_date.getHours().toString();
-		minutes.textContent = diff_as_date.getMinutes().toString();
-		seconds.textContent = diff_as_date.getSeconds().toString();
+			days.textContent = diff_as_date.getDate().toString();
+			hours.textContent = diff_as_date.getHours().toString();
+			minutes.textContent = diff_as_date.getMinutes().toString();
+			seconds.textContent = diff_as_date.getSeconds().toString();
 
-	}, 1000);
+		}, 1000);
+	});
 
 };
--- dist/countdown/ts/countdown.js
+++ dist/countdown/ts/countdown.js
@@ -1,20 +1,27 @@
 window.onload = function () {
-    var countdown = document.querySelector('.countdown');
-    var date = Date.parse(countdown.dataset.date);
-    var days = countdown.querySelector('.days');
-    var hours = countdown.querySelector('.hours');
-    var minutes = countdown.querySelector('.minutes');
-    var seconds = countdown.querySelector('.seconds');
-    setInterval(function () {
-        var diff = date - Date.now();
-        if (diff < 0) {
-            diff = 0;
-        }
-        var diff_as_date = new Date(diff);
-        days.textContent = diff_as_date.getDate().toString();
-        hours.textContent = diff_as_date.getHours().toString();
-        minutes.textContent = diff_as_date.getMinutes().toString();
-        seconds.textContent = diff_as_date.getSeconds().toString();
-    }, 1000);
+    var COUNTDOWN_SELECTOR = '.countdown';
+    var COUNTDOWN_DAYS_SELECTOR = '.days';
+    var COUNTDOWN_HOURS_SELECTOR = '.hours';
+    var COUNTDOWN_MINUTES_SELECTOR = '.minutes';
+    var COUNTDOWN_SECONDS_SELECTOR = '.seconds';
+    var countdowns = document.querySelectorAll(COUNTDOWN_SELECTOR);
+    countdowns.forEach(function (countdown) {
+        var date = Date.parse(countdown.dataset.date);
+        var days = countdown.querySelector(COUNTDOWN_DAYS_SELECTOR);
+        var hours = countdown.querySelector(COUNTDOWN_HOURS_SELECTOR);
+        var minutes = countdown.querySelector(COUNTDOWN_MINUTES_SELECTOR);
+        var seconds = countdown.querySelector(COUNTDOWN_SECONDS_SELECTOR);
+        setInterval(function () {
+            var diff = date - Date.now();
+            if (diff < 0) {
+                diff = 0;
+            }
+            var diff_as_date = new Date(diff);
+            days.textContent = diff_as_date.getDate().toString();
+            hours.textContent = diff_as_date.getHours().toString();
+            minutes.textContent = diff_as_date.getMinutes().toString();
+            seconds.textContent = diff_as_date.getSeconds().toString();
+        }, 1000);
+    });
 };
 //# sourceMappingURL=countdown.js.map
